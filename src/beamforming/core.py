b545c64216f6e9b0934298ef3b4d0cc615b8e5f9
William Jenkins <wfjenkins@ucsd.edu>
2025-05-24 22:10:14 -0400

    Add beamforming base class.

--- src/beamforming/core.py
+++ src/beamforming/core.py
@@ -1,4 +1,54 @@
+from abc import ABC, abstractmethod
 from enum import StrEnum
+
+
+class BeamformerBase(ABC):
+    def __init__(self, array_geometry):
+        """
+        Parameters:
+        -----------
+        array_geometry : ArrayGeometry
+            Geometry of the sensor array
+        """
+        self.array_geometry = array_geometry
+
+    @abstractmethod
+    def process(self, signal, direction):
+        """
+        Process signal to enhance a specific direction
+
+        Parameters:
+        -----------
+        signal : Signal
+            Multichannel input signal
+        direction : ndarray
+            Direction of interest
+
+        Returns:
+        --------
+        Signal
+            Beamformed output signal
+        """
+        pass
+
+    @abstractmethod
+    def response(self, frequencies, directions):
+        """
+        Calculate beampattern/directivity
+
+        Parameters:
+        -----------
+        frequencies : ndarray
+            Frequencies to calculate response for
+        directions : ndarray
+            Directions to calculate response for
+
+        Returns:
+        --------
+        ndarray
+            Beampattern response
+        """
+        pass
 
 
 class CoordinateSystem(StrEnum):
